86d490b11e67fe959200a4257930fdddd52ac405
alyssialopr <alyssia.pradon@gmail.com>
2025-12-12 12:06:36 +0100

    ajout couleurs et  font

---
+++ FRONT/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        raspberry: {
+          50: '#FAEBF4',
+          100: '#F5D6E8',
+          200: '#EBADD1',
+          300: '#E085BA',
+          400: '#D65CA3',
+          500: '#CC338C',
+          600: '#A32970',
+          700: '#7A1F54',
+          800: '#521438',
+          900: '#290A1C',
+          950: '#1D0714',
+        },
+      },
+      fontFamily: {
+        sans: ['Lexend', 'Helvetica Neue', 'Arial', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
