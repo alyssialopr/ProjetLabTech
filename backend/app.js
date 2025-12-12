716ab5cad242ceaf92f06322cb2a5469bfe46e59
CharleryM <charlerymalcolm.l@gmail.com>
2025-12-12 16:28:07 +0100

    server is ok

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
-import express  from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+  console.log(`Example app listening on port ${PORT}`);
+});
+
+// Force le processus à rester vivant
+setInterval(() => {}, 1000);
