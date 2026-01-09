bdc8de2034ddd7de1f93cfb5bca596825619219e
CharleryM <charlerymalcolm.l@gmail.com>
2026-01-09 09:25:27 +0100

    rename FRONT in frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section className="bg-white">
+        <p className="text-raspberry-900">OPIOIOI</p>
+      </section>
+    </>
+  );
+}
+
+export default App;
