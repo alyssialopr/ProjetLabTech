73eafb3d86f26d3cae97ebb389b9be082170f2db
CharleryM <charlerymalcolm.l@gmail.com>
2026-01-29 10:50:08 +0100

    rebase to main

--- FRONT/src/router.jsx
+++ FRONT/src/router.jsx
@@ -1,18 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import LabIALanding from "./pages/LabIALanding";
 import LabResultsPage from "./pages/LabResultsPage";
 import Analysis from "./pages/Analysis";
 import Upload from "./pages/Upload";
 import Manual from "./pages/Manual";
 import ManualValues from "./pages/ManualValues";
+import Help from "./pages/Help";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <Layout><LabIALanding /></Layout> },
-  { path: "/analysis", element: <Layout><Analysis /></Layout> },
-  { path: "/upload", element: <Layout><Upload /></Layout> },
-  { path: "/manual", element: <Layout><Manual /></Layout> },
-  { path: "/manual/values", element: <Layout><ManualValues /></Layout> },
-  { path: "/results", element: <Layout><LabResultsPage /></Layout> }
+  { path: "/", element: <LabIALanding /> },
+  { path: "/analysis", element: <Analysis /> },
+  { path: "/upload", element: <Upload /> },
+  { path: "/manual", element: <Manual /> },
+  { path: "/manual/values", element: <ManualValues />},
+  { path: "/results", element: <LabResultsPage /> },
+  { path: "/help", element: <Help /> }
 ]);
